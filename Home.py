8a1f03a9f4fc626801e602bcb4972cf1d3f5a1ea
Unais2003 <unaispkr3002@gmail.com>
2026-04-15 23:15:08 +0200

    chore : updation of author details

--- Home.py
+++ Home.py
@@ -2,4 +2,21 @@ import streamlit as st
 
 st.set_page_config(page_title="Digital Signal Processing", layout="wide")
 
-st.title("Course: Digital Signal Processing")
+st.markdown("""
+- **Name:** Perinchikkal Muhammed Unais
+- **Institution:** FH Joanneum
+- **Program:** Data Science and Artificial Intelligence
+- **Course Code:** DAT_SS2026_2
+- **Creation Date:** 14 March 2026
+- **Last Updated:** 13 April 2026
+""")
+
+st.title(" My Digital Signal Processing")
+
+st.markdown("""
+Welcome to my Digital Signal Processing (DSP) project! This project is designed to demonstrate various DSP
+techniques and applications. Here, you can explore different signal processing methods, visualize signals, and understand how DSP is used in real-world applications.
+Feel free to navigate through the different sections to learn more about DSP concepts and see them in action. Whether you're a student, a professional, or just curious about signal processing, this project has something for everyone. Enjoy exploring the world of Digital Signal Processing!
+""")
+
+
